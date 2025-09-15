691a31a556cf7058a53aa6cbcc4600f73904aa3c
caitlyntang <caitlyntong6@gmail.com>
2025-09-15 12:41:31 +0800

    Add initial tests for CategoryForm and useCategory

---
+++ client/src/components/Form/CategoryForm.test.js
@@ -0,0 +1,75 @@
+import '@testing-library/jest-dom/extend-expect';
+import React from 'react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
+import { MemoryRouter, Routes, Route } from 'react-router-dom';
+import CategoryForm from './CategoryForm.js';
+
+// Stub and mock input constraints
+var testValue = '';
+const handleSubmit = jest.fn((e) => { 
+    e.preventDefault();
+});
+
+const setValue = jest.fn((value) => {
+    testValue = value
+});
+
+describe('Create New Category Form', () => {
+    it('renders category form', () => {
+        const { getByPlaceholderText } = render(
+            <MemoryRouter initialEntries={['/dashboard/admin/create-category']}>
+                <Routes>
+                    <Route path="/dashboard/admin/create-category"
+                        element={<CategoryForm handleSubmit={handleSubmit} value={testValue} setValue={setValue} />} />
+                </Routes>
+            </MemoryRouter>
+        );
+
+        expect(getByPlaceholderText('Enter new category')).toBeInTheDocument();
+    });
+
+    it('input should be initially empty', () => {
+        const { getByPlaceholderText } = render(
+            <MemoryRouter initialEntries={['/dashboard/admin/create-category']}>
+                <Routes>
+                    <Route path="/dashboard/admin/create-category"
+                        element={<CategoryForm handleSubmit={handleSubmit} value={testValue} setValue={setValue} />} />
+                </Routes>
+            </MemoryRouter>
+        );
+
+        expect(getByPlaceholderText('Enter new category').value).toBe('');
+    })
+
+    it('should allow typing new category name', () => {
+        const { getByPlaceholderText } = render(
+            <MemoryRouter initialEntries={['/dashboard/admin/create-category']}>
+                <Routes>
+                    <Route path="/dashboard/admin/create-category"
+                        element={<CategoryForm handleSubmit={handleSubmit} value={testValue} setValue={setValue} />} />
+                </Routes>
+            </MemoryRouter>
+        );
+
+        fireEvent.change(getByPlaceholderText('Enter new category'), { target: { value: 'test category' } });
+        // expect(getByPlaceholderText('Enter new category').value).toBe('test category');
+        expect(testValue).toBe('test category');
+    });
+
+    it('should create new category successfully', () => {
+        const { getByText, getByPlaceholderText } = render(
+            <MemoryRouter initialEntries={['/dashboard/admin/create-category']}>
+                <Routes>
+                    <Route path="/dashboard/admin/create-category"
+                        element={<CategoryForm handleSubmit={handleSubmit} value={testValue} setValue={setValue} />} />
+                </Routes>
+            </MemoryRouter>
+        );
+
+        fireEvent.change(getByPlaceholderText('Enter new category'), { target: { value: 'test category' } });
+        fireEvent.click(getByText('Submit'));
+        // expect(getByPlaceholderText('Enter new category').value).toBe('test category');
+        expect(testValue).toBe('test category');
+        expect(handleSubmit).toHaveBeenCalled();
+    });
+});
---
+++ client/src/hooks/useCategory.test.js
@@ -0,0 +1,37 @@
+import '@testing-library/jest-dom/extend-expect';
+import axios from "axios";
+import React from 'react';
+import { waitFor, renderHook } from '@testing-library/react';
+import useCategory from './useCategory.js';
+import { beforeEach } from 'node:test';
+
+jest.mock('axios');
+
+describe('try get categories', () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('successful invocation of get categories api', async () => {
+
+        axios.get.mockResolvedValueOnce({
+            data: {
+                "success": true,
+                "message": "All Categories List",
+                "category": [
+                    {
+                        "_id": "66db427fdb0119d9234b27ef",
+                        "name": "Book",
+                        "slug": "book",
+                        "__v": 0
+                    }
+                ]
+            }
+        });
+
+        const { result } = renderHook(() => useCategory());
+
+        await waitFor(() => expect(axios.get).toHaveBeenCalled());
+        expect(axios.get).toHaveBeenCalledWith("/api/v1/category/get-category");
+    });
+});
