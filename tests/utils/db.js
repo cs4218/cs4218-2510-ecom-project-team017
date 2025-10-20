bd4d1a6271b32b604538331a03bea19dfe14956d
Ruth Lim <110249543+ruth-lim@users.noreply.github.com>
2025-10-21 01:41:16 +0800

    Add utils to connect to testDB for integration testing

---
+++ tests/utils/db.js
@@ -0,0 +1,35 @@
+// Generated with aid from ChatGPT
+import mongoose from "mongoose";
+import { MongoMemoryServer } from "mongodb-memory-server";
+
+let mongoServer;
+
+export async function connectToTestDb(dbName = "jest-test") {
+  mongoServer = await MongoMemoryServer.create();
+  const uri = mongoServer.getUri();
+
+  await mongoose.connect(uri, { dbName });
+  return uri;
+}
+
+export async function resetTestDb() {
+  const { collections } = mongoose.connection;
+  const tasks = Object.values(collections).map((collection) =>
+    collection.deleteMany({}).catch(() => {})
+  );
+  await Promise.all(tasks);
+}
+
+export async function disconnectFromTestDb() {
+  try {
+    if (mongoose.connection.readyState) {
+      await mongoose.disconnect();
+    }
+  } catch {}
+  if (mongoServer) {
+    try {
+      await mongoServer.stop();
+    } catch {}
+    mongoServer = undefined;
+  }
+}
